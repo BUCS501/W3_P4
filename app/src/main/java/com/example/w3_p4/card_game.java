55187c2c77da5c151504e596b60a1a0e1cc9a641
Pratham Shroff <pshroff@bu.edu>
2022-09-27 22:10:27 -0400

    Fixed submit button issue, fixed layout, everything works!
    Minor issue of num1 < num2 in subtraction and negative numbers not being accepted to be fixed

--- app/src/main/java/com/example/w3_p4/card_game.java
+++ app/src/main/java/com/example/w3_p4/card_game.java
@@ -23,12 +23,12 @@ public class card_game extends AppCompatActivity {
     private EditText input;
     private int[] num1 = new int[]{-1,0,0,0,0,0,0,0,0,0};
     private int[] num2 = new int[10];
-    private String[] operator = new String[10];
-    private String[] op = new String[]{"+", "-"};
+    private int[] op = new int[10];
     private Integer score = 0;
     private Integer counter = 0;
     private int[] answer = new int[10];
     private int[] ans = new int[10];
+    private int[] correct = new int[10];
 
     public void reload() {
         Intent intent = getIntent();
@@ -55,28 +55,29 @@ public class card_game extends AppCompatActivity {
         Gen.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
                 Random rand = new Random();
 
                 for (int i = 0; i < 10; i++) {
                     num1[i] = rand.nextInt(99) + 1;
                     num2[i] = rand.nextInt(20) + 1;
-                    int tick = rand.nextInt(2);
-                    operator[i] = op[tick];
-                }
+                    op[i] = rand.nextInt(2);
 
-                for (int j = 0; j < 10; j++) {
-                    if (operator[j].equals("+")) {
-                        answer[j] = num1[j] + num2[j];
-                    } else {
-                        answer[j] = num1[j] - num2[j];
+                    if (op[i] == 0) {
+                        answer[i] = num1[i] + num2[i];
+                    } else if (op[i] == 1){
+                        answer[i] = num1[i] - num2[i];
                     }
                 }
 
                 Gen.setEnabled(false);
                 opr1.setText(String.valueOf(num1[counter]));
                 opr2.setText(String.valueOf(num2[counter]));
-                oper.setText(operator[counter]);
+                if (op[counter] == 0) {
+                    oper.setText("+");
+                }
+                else {
+                    oper.setText("-");
+                }
 
             }
         });
@@ -93,34 +94,43 @@ public class card_game extends AppCompatActivity {
                     Toast.makeText(getApplicationContext(), "Please enter the answer", Toast.LENGTH_LONG).show();
                 }
 
+                /*
                 else if (counter == 10){
-
-                    for (int k = 0; k < 10; k++){
-                        if (answer[k] == ans[k]){
-                            score ++;
-                        }
-                    }
 
                     Gen.setEnabled(true);
                     Toast.makeText(getApplicationContext(), "Your score is " + score.toString() + ". Let's play again", Toast.LENGTH_LONG).show();
                     reload();
                 }
-                else{
+                 */
 
+                else{
                     ans[counter] = Integer.parseInt(input.getText().toString());
 
-                    opr1.setText(String.valueOf(num1[counter]));
-                    opr2.setText(String.valueOf(num2[counter]));
-                    oper.setText(operator[counter]);
+                    if (answer[counter] == ans[counter]) correct[counter] = 1;
+                    else correct[counter] = 0;
 
-                    counter ++;
+                    if (counter == 9) {
+                        Gen.setEnabled(true);
 
+                        for (int i = 0; i < 10; i++) correct[0] += correct[i];
+
+                        Toast.makeText(getApplicationContext(), "Your score is " + String.valueOf(correct[0]) + ". Let's play again", Toast.LENGTH_LONG).show();
+                        reload();
+                    }
+                    counter++;
+
+                    if (counter != 10) {
+                        opr1.setText(String.valueOf(num1[counter]));
+                        opr2.setText(String.valueOf(num2[counter]));
+                        if (op[counter] == 0) {
+                            oper.setText("+");
+                        } else {
+                            oper.setText("-");
+                        }
+                    }
                 }
-
-
             }
         });
-
     }
 }
 
